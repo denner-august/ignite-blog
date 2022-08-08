7b4c11335348d4e72a6b3d95d0abe9d34cad2648
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2022-08-08 20:23:16 -0300

    configurações o prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
+import Image from 'next/image';
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+
+import logo from '../../public/Logo.svg';
 
 interface Post {
   uid?: string;
@@ -24,13 +27,28 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home(): JSX.Element {
-//   return <h1>testando</h1>;
-// }
+export default function Home(props: HomeProps): JSX.Element {
+  return (
+    <div className={styles.Container}>
+      <Image src={logo} alt="Picture of the author" width={372} height={80} />
+    </div>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({
+    req: { url: process.env.PRISMIC_API_ENDPOINT },
+  });
 
-//   // TODO
-// };
+  const postsResponse = await prismic.getByType('post');
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      slug: post.uid,
+    };
+  });
+
+  return {
+    props: posts,
+  };
+};
